b1c62be6f99a231526708c74cf4c693216fed230
sacrament <xkinked.tailx@gmail.com>
2020-06-03 03:06:33 +0900

    電話番号バリデーション適当実装

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,86 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <ul class="list"></ul>
   </div>
 </template>
+
+<script>
+import $ from 'jquery'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  mounted() {
+    for (let i = 0; i < 300; i++) {
+      $('.list').append(`
+      <div>
+      <div id="error-${i}" class="error-mess">
+        固定電話10桁〜携帯電話11桁の数値を入力してください！
+      </div>
+      <input type="text" class="input" id="input-${i}" />
+      </div>
+      `)
+      $(`#input-${i}`).on('keyup', function() {
+        let phoneNumber = String($(`#input-${i}`).val())
+        phoneNumber = phoneNumber.replace(/[━.*‐.*―.*－.*\-.*ー.]/gi, '')
+        var pattern = /^(0[5-9]0[0-9]{8}|0[1-9][1-9][0-9]{7})$/
+        console.log(phoneNumber)
+        var flag = phoneNumber.match(pattern)
+        if (flag === null) {
+          document.querySelector(`#error-${i}`).style.display = 'block'
+        } else {
+          document.querySelector(`#error-${i}`).style.display = 'none'
+        }
+        if (phoneNumber === '') {
+          if (phoneNumber === '') {
+            document.querySelector(`#error-${i}`).style.display = 'none'
+          }
+        }
+      })
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.container {
+  width: 100%;
+}
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  height: auto;
+}
+.error-mess {
+  color: red;
+  font-weight: bold;
+  display: none;
+}
+.input {
+  display: block;
+  margin-bottom: 10px;
+  margin-right: 5px;
+  width: 300px;
+  color: black;
+  padding: 8px;
+  border-radius: 5px;
+  border: solid 2px darkcyan;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <ul class="list"></ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import $ from 'jquery'
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  mounted() {
+    for (let i = 0; i < 300; i++) {
+      $('.list').append(
+        `<li class="item" id="item-${i}">${i}番目のリスト要素</li>`
+      )
+      $(`#item-${i}`).on('click', function() {
+        console.log(`私は${i}番目のリスト要素です`)
+        alert(
+          `私が死んでも代わりはいるものと${i}番目の綾波レイが申しております`
+        )
+        $(`#item-${i}`).text('クリックされたよ!!!')
+      })
+    }
+  },
+}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.container {
+  width: 100%;
+}
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  height: auto;
+}
+.list > .item {
+  display: block;
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 300px;
+  color: white;
+  padding: 8px;
+  border-radius: 5px;
+  background-color: darkblue;
+}
+</style>
